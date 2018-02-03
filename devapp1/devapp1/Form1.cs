4c79ba9f4bfb65f2f45de3b4bf6401b20bbda9ee
as ma <amahmood561@gmail.com>
2018-02-03 16:54:46 -0500

    inital commit

---
+++ devapp1/devapp1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace devapp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("test");
+            StringBuilder csvtest = new StringBuilder();
+            csvtest.AppendLine("name,age");
+            csvtest.AppendLine("bob,5");
+            String Pathofcsv = "C:\\Users\\A_mah\\Documents\\test.csv";
+            File.AppendAllText(Pathofcsv, csvtest.ToString());
+
+        }
+    }
+}
